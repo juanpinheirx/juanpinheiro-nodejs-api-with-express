9ad9e76a712ffa88c08551e7a63165723654450d
juanpinheirx <jroliveirapinheiro@gmail.com>
2023-11-10 06:15:50 -0300

    feat: endpoint to get all talker made with test prototype

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const fs = require('fs/promises');
+const { join } = require('path');
 
 const app = express();
+const path = '/talker.json';
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
@@ -10,6 +13,17 @@ const PORT = process.env.PORT || '3001';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.get('/talker', async (req, res) => {
+  const body = req.body;
+  const allTalker = await fs.readFile(join(__dirname, path), 'utf8');
+  const allTalkers = JSON.parse(allTalker);
+  if (body) {
+    return res.status(200).json(allTalkers)
+  } else {
+    return [];
+  }
+})
 
 app.listen(PORT, () => {
   console.log('Online');
